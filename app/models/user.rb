96af921b08100480a7e64174c411c627e4afc181
huatzhi <huatzhi@gmail.com>
2016-12-21 17:41:11 +0800

    add username into user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
 
   validates_presence_of :password, :on => :create
   validates_presence_of :email
+  validates_presence_of :username
   validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", username: "test")
+  end
+
+  it "can be created" do
+    expect(@user).to be_valid
+  end
+
+  it "cannot be created without user" do
+    @user.username = nil
+    expect(@user).to_not be_valid
+  end
 end
