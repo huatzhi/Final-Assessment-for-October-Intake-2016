4a61aad0bed1aa6b9f3fe1e446154bd48a23e0fb
huatzhi <huatzhi@gmail.com>
2016-12-21 14:29:10 +0800

    basic user sign in and out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to login_url, alert: 'Please login first.' if current_user.nil?
+  end
+
+  def authorize_admin
+    redirect_to root_url, alert: 'Only admin can do that.' if !current_user.nil? && current_user.type == 'admin'
+  end
 end
