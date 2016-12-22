0aa65bfdf60ff9823b14a70ef9e00913ee57adf1
huatzhi <huatzhi@gmail.com>
2016-12-22 12:17:18 +0800

    Install bootstrap and fix minor rspec bug

--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe Ad, type: :model do
   describe 'Ad' do
     before do
-      @ad = Ad.create(title:"This is title",price:"2.22",location:'Johor',condition:'first_hand', contact_name:'Mr Huat', phone_number:'123446', email: 'email', description: 'alalallala')
+      AdminUser.create!(username:'admin', email:'admin@huatzhi.com', password:'huatzhi', password_confirmation:'huatzhi')
+      @admin_user = AdminUser.first
+      @ad = Ad.create(title:"This is title",price:"2.22",location:'Johor',condition:'first_hand', contact_name:'Mr Huat', phone_number:'123446', email: 'email', description: 'alalallala', user_id: @admin_user.id)
     end
+    
     it 'can be created' do
       expect(@ad).to be_valid
     end
